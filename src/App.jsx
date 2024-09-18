23186506b521a9054751419cc794d5b6ecd250e6
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-18 10:53:51 +0700

    feat: set item component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
+import "./App.css";
+import Item from './components/item'
 
-import './App.css'
 
 function App() {
-
-
   return (
-    <div className='App'>
-        <h2>React + Vite</h2>
-        <p>Suwanan</p>
+    <div className="App">
+      <h2>React + Vite</h2>
+      <p>Suwanan</p>
+      <Item />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/item.jsx
@@ -0,0 +1,9 @@
+export default function item() {
+  return (
+    <section>
+      <div className="content">
+        <h4>Detail</h4>
+      </div>
+    </section>
+  );
+}
