1ce8475024ca2e0ce758d0df8f19b1566c2f48b5
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-18 12:11:03 +0700

    feat : show and hide

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,14 +1,16 @@
 import './Item.css'
+import {useState} from 'react'
 
 export default function item(props) {
   const {id,title,description} = props
+  const [show , setShow] = useState(false)
   return (
     <section>
       <div className="content">
         <h4>{title}</h4>
-        <button>show</button>
+        <button onClick={()=>setShow(!show)}>{show? "hide" : "show"}</button>
       </div>
-        <p>{description}</p>
+        {show && <p>{description}</p>}
     </section>
   );
 }
